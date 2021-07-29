368f2d88101d42a8d8be5accb826594c28cbd4c3
rikka <se.muromi.rikka@gmail.com>
2021-07-29 17:58:23 +0800

    fix: 🐛 change extends name

--- src/eslint/overrides/angular-html.config.ts
+++ src/eslint/overrides/angular-html.config.ts
@@ -4,6 +4,6 @@ export const ANGULAR_HTML_CONFIG: Linter.ConfigOverride = {
   files: ["*.html"],
   parser: "@angular-eslint/template-parser",
   plugins: ["@angular-eslint/template"],
-  extends: ["plugin:@angular/eslint/template/recommended"],
+  extends: ["plugin:@angular-eslint/template/recommended"],
   rules: {}
 };
