b4a3921fbac55225eb65cc282f8d447b5dee9b73
rikka <admin@rikka.cc>
2021-09-06 17:22:55 +0800

    feat: 🎸 change index export

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
-import { ESLINT_ANGULAR_CONFIG, ESLINT_CONFIG } from "./src/eslint";
-import { COMMON_POSTCSS_CONFIG } from "./src/postcss";
-import { PRETTIER_CONFIG } from "./src/prettier";
-import { STYLELINT_SCSS_CONFIG } from "./src/stylelint";
-import { COMMON_TAILWIND_CONFIG } from "./src/tailwindcss";
-
-export const ESLINT_JS = ESLINT_CONFIG;
-export const PRETTIER_COMMON = PRETTIER_CONFIG;
-export const ESLINT_ANGULAR = ESLINT_ANGULAR_CONFIG;
-export const STYLELINT_SCSS = STYLELINT_SCSS_CONFIG;
-export const TAILWIND_COMMON_CONFIG = COMMON_TAILWIND_CONFIG;
-export const POSTCSS_COMMON_CONFIG = COMMON_POSTCSS_CONFIG;
+export {
+  ESLINT_ANGULAR_CONFIG as ESLINT_ANGULAR,
+  ESLINT_CONFIG as ESLINT_JS
+} from "./src/eslint";
+export * as ESLINT_OVERRIDE from "./src/eslint/overrides";
+export { COMMON_POSTCSS_CONFIG as POSTCSS_COMMON_CONFIG } from "./src/postcss";
+export { PRETTIER_CONFIG as PRETTIER_COMMON } from "./src/prettier";
+export { STYLELINT_SCSS_CONFIG as STYLELINT_SCSS } from "./src/stylelint";
+export { COMMON_TAILWIND_CONFIG as TAILWIND_COMMON_CONFIG } from "./src/tailwindcss";
